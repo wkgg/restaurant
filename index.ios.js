1e9b6201c68ac686e69c1805a8a3b8219b6afb6d
Amos <wky1991@gmail.com>
2015-05-20 23:10:33 +0800

    two tabbar

--- index.ios.js
+++ index.ios.js
@@ -10,14 +10,15 @@ var {
   StyleSheet,
   Text,
   View,
-  ListView
+  ListView,
+  TabBarIOS
 } = React;
 
-var restaurant = React.createClass({
+var List = React.createClass({
   getInitialState: function() {
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     return {
-      dataSource: ds.cloneWithRows(['row 1', 'row 2', 'row 3', 'row 4']),
+      dataSource: ds.cloneWithRows(this.props.data)
     };
   },
 
@@ -25,28 +26,56 @@ var restaurant = React.createClass({
     return (
       <ListView
         dataSource={this.state.dataSource}
-        renderRow={(rowData) => <Text>{rowData}</Text>} />
+        renderRow={(rowData) => <Text style={styles.rowData}>{rowData}</Text>} />
     );
   },
 });
 
+var restaurant = React.createClass({
+  statics: {
+    title: '<TabBarIOS>',
+    description: 'Tab-based navigation.'
+  },
+
+  getInitialState: function() {
+    return {
+      selectedTab: 'blueTab',
+    };
+  },
+  render: function() {
+    return (
+      <TabBarIOS>
+        <TabBarIOS.Item
+          title="Blue Tab"
+          icon={{uri: 'featured'}}
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}>
+          <List data={['row 1', 'row 2', 'row 3', 'row 4']}/>
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          icon={{uri: 'featured'}}
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+            });
+          }}>
+          <List data={['row 5', 'row 6', 'row 7', 'row 8']}/>
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+});
+
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  rowData: {
+    marginTop: 30,
+    fontSize: 25
+  }
 });
 
 AppRegistry.registerComponent('restaurant', () => restaurant);
