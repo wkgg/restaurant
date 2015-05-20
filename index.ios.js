4dbfd7ded27c4f96cd529571cb0ca4a406fcc823
Amos <wky1991@gmail.com>
2015-05-20 22:46:43 +0800

    echo hello world

--- index.ios.js
+++ index.ios.js
@@ -16,16 +16,7 @@ var restaurant = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>Hello World</Text>
       </View>
     );
   }
